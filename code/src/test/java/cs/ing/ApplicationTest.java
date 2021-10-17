1e55e70b4209505ff139a9e94b58f5b7d8767d63
renato <luis.carbajal@utec.edu.pe>
2021-10-16 22:47:17 -0500

    created project

---
+++ code/src/test/java/cs/ing/ApplicationTest.java
@@ -0,0 +1,11 @@
+package cs.ing;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ApplicationTest {
+    @Test
+    public void Test01() {
+        Assert.assertTrue(true);
+    }
+}
